922c29d37bcddcdd5eead10761e6b906b6eb8a5a
longxuan <longxuan@mokahr.com>
2019-10-17 10:47:12 +0800

    Update index export

---
+++ typings/KnexEntity.ts
@@ -0,0 +1,17 @@
+import { Model, ModelDefinition, EntityColumnMap, EntityColumnConvertToEntity, EntityColumnConvertToRaw } from '../src/Model';
+import { DbContext, DbModelDefs, DbModels, DbContextDefinition } from '../src/Context';
+import { EntityNotCreateError } from '../src/Errors';
+
+
+export {
+  Model,
+  ModelDefinition,
+  EntityColumnMap,
+  EntityColumnConvertToEntity,
+  EntityColumnConvertToRaw,
+  DbContext,
+  DbModelDefs,
+  DbModels,
+  DbContextDefinition,
+  EntityNotCreateError,
+};
